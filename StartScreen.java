5d8b75e66ccde1624e841d8bf9fd6bb1872730ba
suvina346 <suvinavijayendran27@gmail.com>
2025-04-02 08:07:39 -0500

    implemented Rules

--- StartScreen.java
+++ StartScreen.java
@@ -7,9 +7,12 @@ public class StartScreen extends JFrame {
 
     private final JLabel startLabel;
     private final JLabel rulesLabel;
+    private final JLabel rulesScreenLabel;
     private final ImageIcon startBtn;
     private final ImageIcon rulesBtn;
     private final int buttonHeight, buttonWidth;
+    private final ImageIcon rulesScreen;
+    public static boolean rulesVisible;
 
     public void startGame()
     {
@@ -28,12 +31,13 @@ public class StartScreen extends JFrame {
 
         startBtn = new ImageIcon(getClass().getResource("/Images/Start/startButton.png"));
         rulesBtn = new ImageIcon(getClass().getResource("/Images/Start/rulesButton.png"));
+        rulesScreen = new ImageIcon(getClass().getResource("/Images/Start/Rules.png"));
         buttonHeight = 156;
         buttonWidth = 436;
 
         startLabel = new JLabel(new ImageIcon(startBtn.getImage().getScaledInstance((int)(buttonWidth/1.5), (int)(buttonHeight/1.5), Image.SCALE_SMOOTH)));
         rulesLabel = new JLabel(new ImageIcon(rulesBtn.getImage().getScaledInstance((int)(buttonWidth/1.5), (int)(buttonHeight/1.5), Image.SCALE_SMOOTH)));
-
+        rulesScreenLabel = new JLabel(new ImageIcon(rulesScreen.getImage().getScaledInstance(getWidth() - 100, getHeight() - 90, Image.SCALE_SMOOTH)));
         startLabel.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -55,6 +59,15 @@ public class StartScreen extends JFrame {
             @Override
             public void mouseClicked(MouseEvent e) {
                 System.out.println("open rules");
+                if (rulesVisible)
+                    return;
+                rulesVisible = true;
+                rulesScreenLabel.setVisible(true);
+                rulesLabel.setVisible(false);
+                startLabel.setVisible(false);
+
+                
+                
             }
 
             @Override
@@ -69,19 +82,39 @@ public class StartScreen extends JFrame {
             }
 
         });
+        rulesScreenLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.out.println("close rules");
+                rulesScreenLabel.setVisible(false);
+                rulesLabel.setVisible(true);
+                startLabel.setVisible(true);
+
+                
+                
+            }
+
+        });
 
         BackgroundPanel panel = new BackgroundPanel();
         panel.setLayout(null);
 
         add(startLabel);
         add(rulesLabel);
+        add(rulesScreenLabel);
         add(panel);
         pack();
         setVisible(true);
         setLocationRelativeTo(null);
 
+        rulesScreenLabel.setBounds(0, -10, getWidth(), getHeight() - 10);
+        rulesScreenLabel.setVisible(false);
+
         startLabel.setBounds(getWidth()-460,250,(int)(buttonWidth/1.5),(int)(buttonHeight/1.5));
         rulesLabel.setBounds(getWidth()-460,350,(int)(buttonWidth/1.5),(int)(buttonHeight/1.5));
+
+        
+
     }
 
     static class BackgroundPanel extends JPanel{
@@ -90,6 +123,8 @@ public class StartScreen extends JFrame {
             super.paintComponent(g);
             ImageIcon bg = new ImageIcon(getClass().getResource("/Images/Start/startScreen.png"));
             g.drawImage(bg.getImage(), -30, 0, getWidth() + 35, getHeight(), this);
+            
+            
         }
     }
 
