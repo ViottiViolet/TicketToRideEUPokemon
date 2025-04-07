9dfdac1d0bcc50e8ae273f9e1ad362e21d0e3580
ViottiViolet <sahara.k2025@gmail.com>
2025-04-07 07:25:12 -0500

    t

--- TrainerIcon.java
+++ TrainerIcon.java
@@ -7,7 +7,7 @@ public class TrainerIcon
 
     private final int h = 280;
 
-    private final ImageIcon profile, train, station;
+    private final ImageIcon profile, bg, train, station;
     private int rank;
 
     private final JLabel bgLabel, profileLabel, nameLabel, trainLabel, stationLabel;
@@ -15,6 +15,7 @@ public class TrainerIcon
     public TrainerIcon(String name, int r, JPanel p)
     {
         profile = new ImageIcon(getClass().getResource("/Images/Trainers/" + name + ".png"));
+        bg = new ImageIcon(getClass().getResource("/Images/Trainers/" + name + "bg.png"));
         train = new ImageIcon(getClass().getResource("/Images/Game/locomotive highlight.png"));
         station = new ImageIcon(getClass().getResource("/Images/Game/station highlight.png"));
         rank = r;
@@ -25,9 +26,7 @@ public class TrainerIcon
 
         profileLabel = new JLabel(new ImageIcon(profile.getImage().getScaledInstance((int)(300/2), (int)(300/2), Image.SCALE_SMOOTH)));
 
-        bgLabel = new JLabel("       ");
-        bgLabel.setOpaque(true);
-        bgLabel.setBackground(Color.RED);
+        bgLabel = new JLabel(new ImageIcon(bg.getImage().getScaledInstance((int)(h - 55), (int)(h/2 + 30), Image.SCALE_SMOOTH)));
 
         trainLabel = new JLabel(new ImageIcon(train.getImage().getScaledInstance((int)(251/4), (int)(201/4), Image.SCALE_SMOOTH)));
         stationLabel = new JLabel(new ImageIcon(station.getImage().getScaledInstance((int)(251/4), (int)(201/4), Image.SCALE_SMOOTH)));
