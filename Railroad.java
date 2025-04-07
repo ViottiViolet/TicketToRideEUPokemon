373b09f31f9ec1190ba01e3f7514e45cb6f52433
Zapdos435 <k1518127@students.katyisd.org>
2025-04-07 08:02:23 -0500

    sdfs

--- Railroad.java
+++ Railroad.java
@@ -1,4 +1,61 @@
 public class Railroad
 {
-    
+
+    City getEnd() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    City getEnd() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    City getEnd() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    private String color;
+    private int length;
+    private boolean isTunnel;
+    private int engineCount;
+    private City a;
+    private City b; 
+    public Railroad (String color, int length, boolean istunnel, int engineCount, City a, City b)
+    {
+        this.color = color;
+        this.length = length;
+        this.isTunnel = istunnel;
+        this.engineCount = engineCount;
+        this.a = a;
+        this.b = b;
+
+        
+    }
+    public Railroad(City a, City b, int weight)
+    {
+        this.a = a; 
+        this.b = b;
+        length = weight;
+    }
+    public String getColor()
+    {
+        return color;
+    }
+
+    public int getLength()
+    {
+        return length;
+    }
+    public boolean isTunnel()
+    {
+        return isTunnel;
+    }
+    public int getEngineCount()
+    {
+        return engineCount;
+    }
+
+    public City getEnd ()
+    {
+        return b;
+    }
+
 }
